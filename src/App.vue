69b9a703a1237d0d2bd1c9c5024f5f856d38962d
3x0De <3x0de.dev@gmail.com>
2026-07-27 21:03:51 +0200

    ˚₊‧꒰ა header ໒꒱ ‧₊˚

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ import { RouterLink, RouterView } from "vue-router";
   <header>
     <nav>
       <RouterLink to="/"><img src="/logos/max.svg" alt="MOPass" /></RouterLink>
+      <div id="Add">
+        <img src="./assets/Add.svg" alt="" height="32" width="32" />
+      </div>
     </nav>
   </header>
 
@@ -13,3 +16,33 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterView />
   </main>
 </template>
+
+<style scoped lang="scss">
+@use "variables" as v;
+
+header {
+  position: sticky;
+  top: 0;
+  background-color: v.$redApple;
+  z-index: 10;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    #Add {
+      height: 100%;
+      aspect-ratio: 1;
+      margin-left: 20px;
+      cursor: pointer;
+      transition: 0.3s all;
+      border-radius: 5px;
+
+      &:hover {
+        background-color: v.$whiteSpirit;
+      }
+    }
+  }
+}
+</style>
