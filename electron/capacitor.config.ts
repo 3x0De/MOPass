89d81bea77e3c6ddf8454489bfbf78c276cd817a
3x0De <3x0de.dev@gmail.com>
2026-07-27 11:38:26 +0200

    ˚₊‧꒰ა Initialisation ໒꒱ ‧₊˚

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'exode.mopass',
+  appName: 'MOPass',
+  webDir: 'dist'
+};
+
+export default config;
