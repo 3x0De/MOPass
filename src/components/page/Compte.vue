a64f755eaa2711e4194981ecda6b0d8da0815022
3x0De <3x0de.dev@gmail.com>
2026-07-28 19:38:42 +0200

    ˚₊‧꒰ა Liste ໒꒱ ‧₊˚

---
+++ src/components/page/Compte.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { ref, watch, computed } from "vue";
+interface Compte {
+  domaine: string;
+  nom?: string;
+}
+
+interface Props {
+  url: number;
+  compte: Compte;
+}
+
+const props = defineProps<Props>();
+
+const currentFavicon = ref<string>("/logos/none.svg");
+
+watch(
+  () => props.compte.domaine,
+  () => {
+    const rawUrl = props.compte.domaine?.trim() || "";
+
+    if (!rawUrl) {
+      currentFavicon.value = "/icons/none.svg";
+      return;
+    }
+
+    if (
+      rawUrl.startsWith("USK@") ||
+      rawUrl.startsWith("KSK@") ||
+      rawUrl.startsWith("SSK@") ||
+      rawUrl.startsWith("CHK@")
+    ) {
+      currentFavicon.value = "/icons/freenet.png";
+      return;
+    }
+
+    try {
+      const parsedUrl = new URL(
+        rawUrl.startsWith("http") ? rawUrl : `https://${rawUrl}`,
+      );
+      const domain = parsedUrl.hostname;
+
+      if (domain.endsWith(".onion")) {
+        currentFavicon.value = "/icons/tor.png";
+        return;
+      }
+      if (domain.endsWith(".loki")) {
+        currentFavicon.value = "/icons/lokinet.jpg";
+        return;
+      }
+      if (domain.endsWith(".i2p")) {
+        currentFavicon.value = "/icons/I2P.jpg";
+        return;
+      }
+
+      const targetFavicon = `${parsedUrl.origin}/favicon.ico`;
+
+      const img = new Image();
+      img.src = targetFavicon;
+
+      img.onload = () => (currentFavicon.value = targetFavicon);
+
+      img.onerror = () => (currentFavicon.value = "/icons/none.svg");
+    } catch {
+      currentFavicon.value = "/icons/none.svg";
+    }
+  },
+  { immediate: true },
+);
+
+function onError() {
+  currentFavicon.value = "/icons/none.svg";
+}
+
+const Name = computed(() => {
+  const rawUrl = props.compte.domaine?.trim() || "";
+
+  try {
+    const parsedUrl = new URL(
+      rawUrl.startsWith("http") ? rawUrl : `https://${rawUrl}`,
+    );
+    const hostname = parsedUrl.hostname.replace(/^www\./, "");
+    const name = hostname.split(".")[0];
+    return name
+      ? name.charAt(0).toUpperCase() + name.slice(1)
+      : props.compte.domaine;
+  } catch {
+    return props.compte.domaine;
+  }
+});
+</script>
+
+<template>
+  <li>
+    <a :href="'/' + String(url)">
+      <img
+        :src="currentFavicon"
+        @error="onError"
+        alt="Favicon"
+        width="16"
+        height="16"
+      />
+      <h2>{{ Name }}</h2>
+      <p>{{ compte.nom }}</p>
+    </a>
+  </li>
+</template>
--- src/components/page/Liste.vue
+++ src/components/page/Liste.vue
@@ -1,114 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import Compte from "./Compte.vue";
+
+interface Compte {
+  domaine: string;
+  nom?: string;
+}
+
+const comptes = ref<Compte[]>([
+  { domaine: "https://google.com" },
+  { domaine: "https://twitch.tv" },
+  { domaine: "https://github.com" },
+  { domaine: "https://strudel.cc" },
+  { domaine: "https://gemini.google.com/app" },
+  { domaine: "https://w3schools.com/vue/vue_v-for.php" },
+  {
+    domaine:
+      "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/",
+  },
+  {
+    domaine:
+      "http://2gzyxa5ihm7nsggfxnu52rck2vv4rvmdlkiu3zzui5du4xyclen53wid.onion/",
+  },
+  { domaine: "https://ahmia.fi/", nom: "aa" },
+  { domaine: "https://ssssssss" },
+  { domaine: "https://lokinet.loki/" },
+  { domaine: "http://zzz.i2p" },
+  {
+    domaine:
+      "USK@g29a73m3sCq2A-M~U5tP-RjX~6eE4gX11Y2E~01G5A0,9X1Y8Z7W6V5U4T3S2R1Q,AQACAAE/social-guide/1/",
+  },
+]);
+
+comptes.value.sort();
+</script>
 
 <template>
   <nav>
     <ul>
-      <li>hello 1</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world</li>
-      <li>hello</li>
-      <li>world fin</li>
+      <Compte v-for="(value, i) in comptes" :compte="value" :url="i" />
     </ul>
   </nav>
 </template>
